05470da95035e7114b477ff547aad1f83035737c
William Pyle <wt.pyle@gmail.com>
2020-06-12 14:41:37 -0400

    Cleaned up code.

--- Object-Pooling/Assets/Scripts/PoolManager.cs
+++ Object-Pooling/Assets/Scripts/PoolManager.cs
@@ -2,28 +2,75 @@
 using UnityEngine;
 
 /// <summary>
-/// TODO:
-/// -Check to make sure when an object is requested that the pool has an available object to give
-///     -If it doesn't create a new object (if the pool is expandable, otherwise I guess throw an error and return null?)
-/// -Change the dict to reference a class that contains a queue so that you also have access to other info.
-///         -How many objects are a part of that pool (checked out and not)
-///         -if the pool is expandable
-///         -The max amount the pool can expand to
-/// 
-///     
-/// -Pools that contain more than one type of game object spawn objects into the pool randomly. Should I offer extra options as to how
-///     objects are spawned into the pool?
+/// Handles pools of objects. Other classes should use the pool manager to request pooled objects.
 /// </summary>
 public class PoolManager : MonoBehaviour
 {
+
+    public static PoolManager Instance { get; private set; }
+
+    [SerializeField] private PoolInfo[] poolInfos;
+
+    private Dictionary<string, Pool> pools = new Dictionary<string, Pool>();
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+        GeneratePools();
+    }
+
+    private void GeneratePools()
+    {
+        foreach(var info in poolInfos)
+        {
+            Pool pool = new Pool(info, this.transform);
+            pools.Add(info.ID, pool);
+        }
+    }
+
+    /// <summary>
+    /// Call to retrieve a Gameobject from a pool.
+    /// </summary>
+    /// <param name="key">Which pool you want to gameobject to come from.</param>
+    /// <returns></returns>
+    public GameObject CheckoutItem(string key) => pools[key].RequestFromQueue();
+
+    /// <summary>
+    /// Pass an object back into the pool it came from. Object's not created from a pool will be destroyed.
+    /// </summary>
+    /// <param name="gameObject">Object to return.</param>
+    public void ReturnItem(GameObject gameObject)
+    {
+        PoolTag tag = gameObject.GetComponent<PoolTag>();
+        if (tag != null)
+        {
+            pools[tag.ItemTag].ReturnItem(gameObject);
+        }
+        else
+        {
+            Debug.LogError($"Warning: Trying to return object {gameObject.name} without pool tag. Object has been destroyed. " +
+                $"Make sure you are not destroying the tag or trying to return an object that never belonged to a pool.");
+            Destroy(gameObject);
+        }
+    }
+
+
     private class Pool
     {
         private PoolInfo info;
 
         private Transform poolParent;
-        private int nameCounter = 0; //Just for incrementing the name of the pool member
-        
-        //The total amount of objects that are a part of this pool. Includes objects that have been checked out
+        //For incrementing the name of the pool member
+        private int nameCounter = 0; 
+        //The total amount of objects that are a part of this pool. Includes objects that have been checked out.
         private int currentPoolSize = 0;
 
         private Queue<GameObject> queue = new Queue<GameObject>();
@@ -32,6 +79,7 @@ public class PoolManager : MonoBehaviour
         {
             this.info = poolInfo;
 
+            //Setup transform that will be used as the parent for all pool members
             poolParent = new GameObject().transform;
             poolParent.name = poolInfo.ID;
             poolParent.SetParent(parentTransform);
@@ -41,7 +89,6 @@ public class PoolManager : MonoBehaviour
 
         private void Init()
         {
-            //Create GameObjects in queue
             for (int i = 0; i < info.poolSize; i++)
             {
                 CreatePoolMember();
@@ -55,11 +102,11 @@ public class PoolManager : MonoBehaviour
                     Vector3.zero,
                     Quaternion.identity);
 
-            newObj.name = info.ID + " " + nameCounter.ToString();
-            nameCounter++;
             newObj.SetActive(false);
             newObj.AddComponent<PoolTag>().Init(info.ID);
             newObj.transform.SetParent(poolParent);
+            newObj.name = info.ID + " " + nameCounter.ToString();
+            nameCounter++;
 
             AddToQueue(newObj);
         }
@@ -70,6 +117,8 @@ public class PoolManager : MonoBehaviour
             queue.Enqueue(obj);
         }
 
+        
+        //HACK?: Both of these methods are only called by the Pool Manager and it seems kind of weird
         public GameObject RequestFromQueue()
         {
             GameObject obj = null;
@@ -92,54 +141,24 @@ public class PoolManager : MonoBehaviour
             }
             return obj;
         }
-
         public void ReturnItem(GameObject gameObject) => queue.Enqueue(gameObject);
 
     }
+}
 
-    public static PoolManager Instance { get; private set; }
+/// <summary>
+/// This component is added to all GameObjects that are created as a part of a pool.
+/// It IDs which pool the object came from originally so that it may be returned later.
+/// </summary>
+public class PoolTag : MonoBehaviour
+{
+    //Item tag is the dictionary key of the pool it came from
+    private string itemTag;
+    public string ItemTag => itemTag;
 
-    public PoolInfo[] poolInfos;
-
-    private Dictionary<string, Pool> pools = new Dictionary<string, Pool>();
-
-    private void Awake()
+    public void Init(string tag)
     {
-        if(Instance == null)
-        {
-            Instance = this;
-        }
-        else { Destroy(this); }
-
-        GeneratePools();
-    }
-
-    private void GeneratePools()
-    {
-        foreach(var info in poolInfos)
-        {
-            Pool pool = new Pool(info, this.transform);
-            pools.Add(info.ID, pool);
-        }
-    }
-
-    //TODO: Will need to do more work here to assure we have items to give out
-    public GameObject CheckoutItem(string key) => pools[key].RequestFromQueue();
-
-    public void ReturnItem(GameObject gameObject)
-    {
-        PoolTag tag = gameObject.GetComponent<PoolTag>();
-        if (tag != null)
-        {
-            pools[tag.ItemTag].ReturnItem(gameObject);
-        }
-        else
-        {
-            Debug.LogError($"Warning: Trying to return object {gameObject.name} without pool tag. Object has been destroyed. " +
-                $"Make sure you are not destroying the tag or trying to return an object that never belonged to a pool.");
-            Destroy(gameObject);
-        }
-            
+        itemTag = tag;
     }
 }
 
